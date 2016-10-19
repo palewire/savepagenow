58d0b8fc9f33090c6bd9bc4983a8e6d2ccccc5ee
N0taN3rd <n0tan3rd@gmail.com>
2016-10-19 00:17:46 -0400

    Added webcitation.org and archive.is to the available archives Updated unit tests to reflect these changes

--- test.py
+++ test.py
@@ -5,10 +5,15 @@ import savepagenow
 
 
 class CaptureTest(unittest.TestCase):
-
     def test_attr(self):
         archive_url = savepagenow.capture("http://www.example.com/")
         self.assertTrue(archive_url.startswith("http://web.archive.org/"))
+        archive_url = savepagenow.capture("http://www.example.com/",
+                                          archive='webcitation.org')
+        self.assertTrue(archive_url.startswith("http://www.webcitation.org"))
+        archive_url = savepagenow.capture("http://www.example.com/",
+                                          archive='archive.is')
+        self.assertTrue(archive_url.startswith("http://archive.is"))
 
 
 if __name__ == '__main__':
