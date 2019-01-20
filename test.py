b4ab553d91c3e741e8ff8045d149f6d6437e33e2
Brandon Galbraith <brandongalbraith@users.noreply.github.com>
2019-01-20 17:18:29 -0500

    Update test to correspond to scheme change

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class CaptureTest(unittest.TestCase):
         random_number = random.choice(range(0, 1000))
         url = "http://www.example.com/my-random-page-{}".format(random_number)
         archive_url_1, c1 = savepagenow.capture_or_cache(url)
-        self.assertTrue(archive_url_1.startswith("http://web.archive.org/"))
+        self.assertTrue(archive_url_1.startswith("https://web.archive.org/"))
 
         # Test CacheError
         archive_url_2, c2 = savepagenow.capture_or_cache(
