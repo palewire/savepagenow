72f0e364d2860fd6458cbdaab2b4a0f7422a6c68
s <102841251+duckduckgrayduck@users.noreply.github.com>
2023-06-30 13:51:03 -0500

    Add unit test for capture with authentication flag

--- tests/test_capture.py
+++ tests/test_capture.py
@@ -1,8 +1,15 @@
 import savepagenow
 
-
 def test_capture():
     """Test the basic function of retriving a URL from Wayback."""
     url = "https://www.latimes.com/"
     archive_url, c = savepagenow.capture_or_cache(url)
+    assert archive_url.startswith("https://web.archive.org/")
+
+def test_auth_capture():
+    """Test the retrieval of URL from Wayback with authentication"""
+    url = "https://www.latimes.com/"
+    # The env variables get pulled when we call capture()
+    # You have to set access_key and secret to the appropriate values for your archive.org account
+    archive_url, c = savepagenow.capture(url, authenticate=True)
     assert archive_url.startswith("https://web.archive.org/")
