7dcd52a35f80d7b5a46a1b8617fb83ab4217f962
palewire <b@palewi.re>
2023-07-01 08:22:56 -0400

    Trim

--- savepagenow/__init__.py
+++ savepagenow/__init__.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
 from .api import BlockedByRobots, CachedPage, capture, capture_or_cache
-
-__version__ = "1.2.3"
 
 __all__ = ("BlockedByRobots", "capture", "capture_or_cache", "CachedPage")
