2af8330ba3cd9acbbc58d04e444addd405ce7310
palewire <b@palewi.re>
2023-07-01 09:01:58 -0400

    More URLs

--- tests/test_capture.py
+++ tests/test_capture.py
@@ -1,15 +1,27 @@
+import random
+
 import savepagenow
+
+SITE_LIST = [
+    "https://palewi.re/docs/first-django-admin/",
+    "https://palewi.re/docs/first-github-scraper/",
+    "https://palewi.re/docs/first-pull-request/",
+    "https://palewi.re/docs/first-python-notebook/",
+    "https://palewi.re/docs/first-visual-story/",
+    "https://palewi.re/docs/air-quality-index/",
+    "https://bln-python-client.readthedocs.io/",
+]
 
 
 def test_capture():
     """Test the basic function of retriving a URL from Wayback."""
-    url = "https://www.reuters.com/"
+    url = random.choice(SITE_LIST)
     archive_url, c = savepagenow.capture_or_cache(url)
     assert archive_url.startswith("https://web.archive.org/")
 
 
 def test_auth_capture():
     """Test the retrieval of URL from Wayback with authentication."""
-    url = "https://www.latimes.com/"
+    url = random.choice(SITE_LIST)
     archive_url, c = savepagenow.capture(url, authenticate=True)
     assert archive_url.startswith("https://web.archive.org/")
