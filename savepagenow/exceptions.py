821613c0e63618aa00fdeacc47d850d875d95a3b
palewire <b@palewi.re>
2023-07-01 08:22:17 -0400

    Trim docs

--- savepagenow/exceptions.py
+++ savepagenow/exceptions.py
@@ -5,42 +5,42 @@ class CachedPage(Exception):
 
 
 class WaybackRuntimeError(Exception):
-    """An error returned by the Wayback Machine."""
+    """A generic error returned by the Wayback Machine."""
 
     pass
 
 
 class BlockedByRobots(WaybackRuntimeError):
-    """Raised when archive.org has been blocked by the site's robots.txt access control instructions."""
+    """Raised when archive.org has been blocked by the site's robots.txt."""
 
     pass
 
 
 class BadGateway(WaybackRuntimeError):
-    """Raised when archive.org when you receive a 502 bad gateway status code in response to your request."""
+    """Raised when you receive a 502 bad gateway status code."""
 
     pass
 
 
 class Unauthorized(WaybackRuntimeError):
-    """Raised when archive.org when you receive a 401 unauthorized status code in response to your request."""
+    """Raised when you receive a 401 unauthorized status code."""
 
     pass
 
 
 class Forbidden(WaybackRuntimeError):
-    """Raised when archive.org when you receive a 403 forbidden status code in response to your request."""
+    """Raised when you receive a 403 forbidden status code."""
 
     pass
 
 
 class TooManyRequests(WaybackRuntimeError):
-    """Raised when archive.org when you have exceeded its throttle on request frequency. Slow it down."""
+    """Raised when you have exceeded the throttle on request frequency."""
 
     pass
 
 
 class UnknownError(WaybackRuntimeError):
-    """Raised when archive.org when you receive a 520 unknown status code in response to your request."""
+    """Raised when you receive a 520 unknown status code."""
 
     pass
