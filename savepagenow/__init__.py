c06b4388df7b0f9e9f47d894a27d7e8f2eb28e34
Ben <Ben.Welsh@thomsonreuters.com>
2026-01-19 06:48:31 -0500

    Fix formatting in __init__.py to maintain consistent indentation for exported members

--- savepagenow/__init__.py
+++ savepagenow/__init__.py
@@ -1,8 +1,8 @@
 from .api import BlockedByRobots, CachedPage, capture, capture_or_cache
 
 __all__ = (
-	"BlockedByRobots",
-	"capture",
-	"capture_or_cache",
-	"CachedPage",
+    "BlockedByRobots",
+    "capture",
+    "capture_or_cache",
+    "CachedPage",
 )
